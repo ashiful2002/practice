b90df2cb363200200f1b5b340d896f912b49a18a
ashiful-islam <ashifulislam2002@gmail.com>
2024-09-10 18:37:02 +0600

    icon allignment

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,13 +10,13 @@ import { FaLinkedin } from "react-icons/fa";
 const Navbar = () => {
     return (
         <>
-            <nav className=' flex justify-between items-center mb-20 mr-0'>
+            <nav className=' flex justify-between items-center mb-20 mt-1'>
                 <div className='flex items-center flex-shrink-0'>
                     <a href="/">
-                    <img src={HelloSheLogo}  className='mx-2 w-20 rounded-full' alt="logo" />
-                    </a>    
+                        <img src={HelloSheLogo} className='mx-2 w-20 rounded-full' alt="logo" />
+                    </a>
                 </div>
-                <ul className='m-8 flex items-center justify-center gap-4 text-2xl'>
+                <ul className='my-8 flex items-center justify-center gap-4 text-2xl'>
                     <li><a href="https://www.facebook.com/profile.php?id=61565501011722&mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer"><FaFacebookSquare /></a></li>
                     <li><a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"> <FaInstagramSquare /></a></li>
                     <li><a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer"> <FaYoutube /></a></li>
