d0b2da3b0a4abaf4ad0f05cbe11a2acba919cc8a
ashiful-islam <ashifulislam2002@gmail.com>
2024-09-13 17:50:21 +0600

    content upload

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='flex flex-col gap-3 lg:flex-row lg:justify-between text-center'>
             <div>
-                <p>Copyright © 2020 | all rights reserved by Hello She </p>
+                <p>Copyright © 2024 | all rights reserved by Hello She </p>
             </div>
             <div className='text-center text-sm '>
                 <p>Developoed by <a href="https://m.me/ashifulislam.mukto" target='_blank'> <span className='underline text-blue-300'>Mukto</span> </a></p>
